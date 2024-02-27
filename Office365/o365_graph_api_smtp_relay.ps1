9648fe0d4734e533ba7b4f37e02c1a4ae3a024aa
HyperPrime51 <dyoung213@gmail.com>
2024-02-26 17:44:08 -0800

    update

---
+++ Office365/o365_graph_api_smtp_relay.ps1
@@ -0,0 +1,66 @@
+# app registration in Azure variable, app registration needs Mail.Send permission
+$clientID = "client id"
+$Clientsecret = "client secret"
+$tenantID = "tenantid"
+
+$MailSender = "sender"
+$MailRecipient = "recipient"
+
+$Attachment = "full path of attachment if needed, ie: c:\temp\file1.csv"
+#Get File Name and Base64 string
+$FileName=(Get-Item -Path $Attachment).name
+$base64string = [Convert]::ToBase64String([IO.File]::ReadAllBytes($Attachment))
+
+
+
+
+
+#Connect to GRAPH API
+$tokenBody = @{
+    Grant_Type    = "client_credentials"
+    Scope         = "https://graph.microsoft.com/.default"
+    Client_Id     = $clientId
+    Client_Secret = $clientSecret
+}
+$tokenResponse = Invoke-RestMethod -Uri "https://login.microsoftonline.com/$tenantID/oauth2/v2.0/token" -Method POST -Body $tokenBody
+$headers = @{
+    "Authorization" = "Bearer $($tokenResponse.access_token)"
+    "Content-type"  = "application/json"
+}
+
+#Send Mail    
+$URLsend = "https://graph.microsoft.com/v1.0/users/$MailSender/sendMail"
+$BodyJsonsend = @"
+                    {
+                        "message": {
+                          "subject": "Hello World from Microsoft Graph API",
+                          "body": {
+                            "contentType": "HTML",
+                            "content": "This Mail is sent via Microsoft <br>
+                            GRAPH <br>
+                            API<br>
+                            and an Attachment <br>
+                            "
+                          },
+                          
+                          "toRecipients": [
+                            {
+                              "emailAddress": {
+                                "address": "$Recipient"
+                              }
+                            }
+                          ]
+                          ,"attachments": [
+                            {
+                              "@odata.type": "#microsoft.graph.fileAttachment",
+                              "name": "$FileName",
+                              "contentType": "text/plain",
+                              "contentBytes": "$base64string"
+                            }
+                          ]
+                        },
+                        "saveToSentItems": "false"
+                      }
+"@
+
+Invoke-RestMethod -Method POST -Uri $URLsend -Headers $headers -Body $BodyJsonsend
