9e3a9d73b3684b6d71134e7f81c8b9c83e62d3ee
HyperPrime51 <dyoung213@gmail.com>
2024-09-30 20:28:03 -0700

    commit

--- RDS/user_profile_migration.ps1
+++ RDS/user_profile_migration.ps1
@@ -1,34 +1,38 @@
 # variables
 $source = "F:"
-$target = "\\rds-02\c$\users\clee"
+$target = "\\rds-03\c$\users\clee"
 $target_drive = "G:"
 
 # desktop
-Copy-item -Force -Recurse -Verbose -Path "$($source)\desktop" -Destination "$($target)\desktop"
+Copy-item -Force -Recurse -Verbose -Path "$($source)\desktop" -Destination "$($target)"
 
 # documents
-Copy-item -Force -Recurse -Verbose -Path "$($source)\documents" -Destination "$($target)\documents"
+Copy-item -Force -Recurse -Verbose -Path "$($source)\documents" -Destination "$($target)"
 
 # downloads
-Copy-item -Force -Recurse -Verbose -Path "$($source)\downloads" -Destination "$($target)\downloads"
+Copy-item -Force -Recurse -Verbose -Path "$($source)\downloads" -Destination "$($target)"
 
 # favorites
-Copy-item -Force -Recurse -Verbose -Path "$($source)\favorites" -Destination "$($target)\favorites"
+Copy-item -Force -Recurse -Verbose -Path "$($source)\favorites" -Destination "$($target)"
 
 # chrome favorites
 Copy-item -Force -Verbose -Path "$($source)\AppData\Local\Google\Chrome\User Data\Default\Bookmarks" -Destination "$($target)\AppData\Local\Google\Chrome\User Data\Bookmarks"
 Copy-item -Force -Verbose -Path "$($source)\AppData\Local\Google\Chrome\User Data\Default\Bookmarks.bak" -Destination "$($target)\AppData\Local\Google\Chrome\User Data\Bookmarks.bak"
 
 # pinned folders
-Copy-item -Force -Recurse -Verbose -Path "$($source)\AppData\Roaming\Microsoft\Windows\Recent\AutomaticDestinations" -Destination "$($target)\AppData\Roaming\Microsoft\Windows\Recent\AutomaticDestinations"
+Copy-item -Force -Recurse -Verbose -Path "$($source)\AppData\Roaming\Microsoft\Windows\Recent\AutomaticDestinations" -Destination "$($target)\AppData\Roaming\Microsoft\Windows\Recent"
 
 # pinned app in taskbar
 # export
 Import-RegistryHive -File "$($source)\NTUSER.DAT" -Key 'HKU\TEMP_SOURCE_HIVE' -Name TempSourceHive
-REG EXPORT HKEY_USER\TEMP_HIVE\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband "$($target)\tb-pinned-items.reg"
-Copy-item -Force -Recurse -Verbose -Path "$($source)\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar" -Destination "$($target)\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar"
+REG EXPORT HKEY_USERS\TempSourceHive\Software\Microsoft\Windows\CurrentVersion\Explorer\Taskband "$($target)\tb-pinned-items.reg"
+# replace HKEY_USERS\TempSourceHive with HKEY_CURRENT_USER
+Copy-item -Force -Recurse -Verbose -Path "$($source)\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned\TaskBar" -Destination "$($target)\AppData\Roaming\Microsoft\Internet Explorer\Quick Launch\User Pinned"
 Remove-RegistryHive -Name TempSourceHive
 
 # import
 # needs to run under user profile
 REGEDIT /S "%userprofile%\tb-pinned-items.reg"
+
+# new teams
+# path to startup C:\Users\clee\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
